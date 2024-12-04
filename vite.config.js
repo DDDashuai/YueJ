0e10decede60301c73b554222eb9a321ebe8efcf
YYYueJ <15102240+yyyuej@user.noreply.gitee.com>
2024-12-04 19:51:21 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd())
-  
-  return {
-    plugins: [vue()],
-    base: env.VITE_BASE_URL,
-    server: {
-      port: 3000,
-      proxy: mode === 'local' ? {
-        '/api': {
-          target: 'http://localhost:8080',
-          changeOrigin: true
-        }
-      } : {}
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src')
-      }
-    }
-  }
+export default defineConfig({
+  plugins: [vue()],
+  base: '/YueJ/',
 })
